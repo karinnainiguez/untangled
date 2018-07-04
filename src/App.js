94af781d617761bd404daad668bd697d0e6af828
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-03 21:22:57 -0700

    initial commit of react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Untangled</h1>
+        </header>
+        <p>Welcome.  Pick a starting and ending points from Wikipedia.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
