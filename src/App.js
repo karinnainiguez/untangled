7bc4b4c0a8fc9defcbc389cced66556020ba3d75
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-05 18:15:23 -0700

    additional styling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ class App extends Component {
         </header>
 				<h4>Welcome to UnTangled!</h4>
         <p>
-					Enter your starting Wiki page and ending Wiki page.  Our spidey sense will determine the shortest path between them.  Try to achieve it yourself, or give up to our awesome power and display the best paths!
+					Enter your starting Wiki page and
+					ending Wiki page.  Our spidey sense
+					will determine the shortest path
+					between them.  Try to achieve it
+					yourself, or give up to our awesome
+					power and display the best paths!
 				</p>
 				<SearchForm />
       </div>
