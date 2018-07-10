97556cfb6129aaca5f4e6d0ec070c945b728ae98
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-10 14:56:46 -0700

    Make search form a controlled form with onInputChange and onFormSubmit.  No functionality yet

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -2,17 +2,44 @@ import React, { Component } from 'react';
 import './SearchForm.css';
 
 class SearchForm extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			starting: "",
+			ending: ""
+		}
+	}
+
+	onInputChange = (event) => {
+		let updatedState = {};
+
+		let name = event.target.name;
+		updatedState[name] = event.target.value;
+
+		this.setState( updatedState );
+	}
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
+	}
+
 	render(){
 		return (
-			<form className="search-form">
+			<form className="search-form" onSubmit={this.onFormSubmit}>
 				<div>
 					<input
+						value={this.state.starting}
+						onChange={this.onInputChange}
 						type="text"
 						name="starting"
 						placeholder="Start"
 						className="search-input"
 						/>
 					<input
+						value={this.state.ending}
+						onChange={this.onInputChange}
 						type="text"
 						name="ending"
 						placeholder="End"
