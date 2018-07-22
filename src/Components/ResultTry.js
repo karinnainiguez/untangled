df85baea8b763cd2123edd48be3ae904dacbc29d
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-22 16:21:06 -0700

    Add iframes to try-it section

--- src/Components/ResultTry.js
+++ src/Components/ResultTry.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-
+import PropTypes from 'prop-types';
 
 class ResultTry extends Component {
+
+	static propTypes = {
+		data: PropTypes.object
+	}
 
 
 
 	render(){
-		return (<h3>THIS IS THE RESULT TRY</h3>)
+		return (<div className="try-section">
+			<iframe
+				src="https://en.m.wikipedia.org/wiki/Ada_Developers_Academy"
+				className="try-child"
+				/>
+			<iframe
+				src="https://en.m.wikipedia.org/wiki/Java_(programming_language)"
+				className="try-child"
+				/>
+		</div>);
 	}
 }
 
