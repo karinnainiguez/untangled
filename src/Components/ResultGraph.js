13b8d5ec5f531b68478f124110ee7563e5bfe9ac
Karinna Iniguez <karinnainiguez@gmail.com>
2018-07-25 13:28:06 -0700

    passing in nodes and edges.  not showing direction yet

--- src/Components/ResultGraph.js
+++ src/Components/ResultGraph.js
@@ -8,30 +8,53 @@ class ResultGraph extends Component {
 		data: PropTypes.array
 	}
 
-
-
-
-
 	render(){
+		const nodeArray = []; const linkArray = []; const addedNodes = [];
+		const wholeData = this.props.data;
+
+		for(let i = 0 ; i < wholeData.length ; i += 1 ){
+
+			for(let j = 0; j < wholeData[i]["nodes(path)"].length ; j += 1 ){
+				let node = wholeData[i]["nodes(path)"][j]["title"];
+				if ( !addedNodes.includes(node) ) {
+					nodeArray.push( {'id': node} );
+					addedNodes.push(node);
+				}
+			}
+
+			for (let k = 0; k < (wholeData[i]['nodes(path)'].length - 1) ; k += 1 ) {
+				linkArray.push( {
+					"source": wholeData[i]['nodes(path)'][k]['title'],
+					"target": wholeData[i]['nodes(path)'][k + 1]['title']
+				});
+			}
+
+		}
 
 		const data = {
-			nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
-			links: [{ source: 'Harry', target: 'Sally' }, { source: 'Harry', target: 'Alice' }]
+			nodes: nodeArray,
+			links: linkArray
 		}
 
 		const myConfig = {
+			height: 200,
 			nodeHighlightBehavior: true,
 			node: {
-				color: 'lightgreen',
-				size: 120,
-				highlightStrokeColor: 'blue'
+				color: 'whitesmoke',
+				size: 200,
+				fontSize: 15,
+				fontColor: 'whitesmoke',
+				fontWeight: 'bold',
+				highlightStrokeColor: 'pink',
+				highlightFontSize: 30
 			},
 			link: {
-				highlightColor: 'lightblue'
+				highlightColor: 'pink'
 			}
 		}
 
 		return (
+
 			<div>
 				<h3> HELLO FROM RESULT GRAPH</h3>
 				<Graph
